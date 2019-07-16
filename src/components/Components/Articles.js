e2fb4dcca6381e5c1a4688e09d0df48fbc71b36d
Kate Bryan <katebryan31@gmail.com>
2019-07-16 12:45:54 +0100

    error handling functionality added

--- src/components/Components/Articles.js
+++ src/components/Components/Articles.js
@@ -1,8 +1,65 @@
 import React from "react";
+import * as api from "../../api";
+import Loading from "./Loading";
+import ErrorHandler from "../ErrorHandling/ErrorHandler";
+import TopRatedArticles from "./TopRatedArticles";
 
 class Articles extends React.Component {
+  state = {
+    articles: null,
+    isLoading: true,
+    err: null
+  };
+
   render() {
-    return <h3>an array of articles... eventually</h3>;
+    const { articles, isLoading, err } = this.state;
+    if (err) return <ErrorHandler err={err} />;
+    if (isLoading) return <Loading />;
+    return (
+      <>
+        <h2>Articles</h2>
+        {articles ? (
+          articles.map(
+            ({
+              author,
+              title,
+              article_id,
+              topic,
+              created_at,
+              votes,
+              comment_count
+            }) => {
+              const time = created_at.slice(0, 10);
+              return (
+                <div key={article_id}>
+                  <ul>
+                    <h3>{title}</h3>
+                    <h3>{author}</h3>
+                    <h3>{topic}</h3>
+                    <h3>{time}</h3>
+                    <h3>{votes}</h3>
+                    <h3>{comment_count}</h3>
+                  </ul>
+                </div>
+              );
+            }
+          )
+        ) : (
+          <h2>Loading articles...</h2>
+        )}
+      </>
+    );
+  }
+
+  componentDidMount() {
+    api
+      .getArticles()
+      .then(articles => {
+        this.setState({ articles, isLoading: false });
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
   }
 }
 
