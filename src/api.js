8c6aed451b6c591b97cb7afc063418f8bf622780
Kate Bryan <katebryan31@gmail.com>
2019-07-17 09:20:55 +0100

    parameters added for getArticles request, ability to filter

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "https://ncnews-round.herokuapp.com/api";
 
-export const getArticles = async () => {
-  const { data } = await axios.get(`${BASE_URL}/articles`);
+export const getArticles = async ({ sort_by, topic, order, author, limit }) => {
+  const { data } = await axios.get(`${BASE_URL}/articles`, {
+    params: { sort_by, topic, order, author, limit }
+  });
   return data.articles;
 };
