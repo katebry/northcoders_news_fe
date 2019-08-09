536da7623d5c16e1b88167785c53ec2f31c7918f
Kate Bryan <katebryan31@gmail.com>
2019-08-09 09:05:19 +0100

    further styling introduced to error messages using styled components

--- src/components/ErrorHandling/ErrorHandler.js
+++ src/components/ErrorHandling/ErrorHandler.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  margin-left: 2em;
+  margin-right: 2em;
+  margin-bottom: 2em;
+  padding: 1em;
+  background: rgba(255, 255, 255, 0.75);
+  border: 2px;
+  border-radius: 5px;
+`;
 
 const ErrorHandler = ({ err }) => {
-  return <h1>Oops, something went wrong... {err.message}</h1>;
+  return <H1>Oops, something went wrong... {err.message} 😬</H1>;
 };
 
 export default ErrorHandler;
--- src/components/ErrorHandling/NoMatch.js
+++ src/components/ErrorHandling/NoMatch.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  margin-left: 2em;
+  margin-right: 2em;
+  margin-bottom: 1em;
+  padding: 1em;
+  background: rgba(255, 255, 255, 0.75);
+  border: 2px;
+  border-radius: 5px;
+`;
+
+const H2 = styled.h2`
+  display: flex;
+  justify-content: center;
+`;
 
 const NoMatch = ({ status, msg }) => {
   return (
-    <div className="NoMatch">
-      <h1>
-        Error: {status} {msg}
-      </h1>
-      <h1>Sorry but we could not find what you were looking for...</h1>
-      <Link to="/">
-        <h1>Go back to the Home Page</h1>
-      </Link>
-    </div>
+    <>
+      <H1>
+        Error: {status} {msg}. Sorry but we could not find what you were looking
+        for... 🙅‍
+      </H1>
+      <H2>
+        <Link to="/"> Go back to the Home Page </Link> 🏡
+      </H2>
+    </>
   );
 };
 
